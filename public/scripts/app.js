246545e417ff85fcc23fb75515cb163584963c2e
Denis Nefedov <nefedov_de@soshace.com>
2016-01-13 19:29:11 +0300

    Site menu added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,17 +19,25 @@ var App = React.createClass({
 var SiteMenu = React.createClass({
     render: function() {
         return (
-            <div className="siteMenu">
-                <ul>
-                    <li><Link to="index">Index</Link></li>
-                    <li><Link to="about">About</Link></li>
-                    <li><Link to="services">Services</Link></li>
-                    <li><Link to="work">Work</Link></li>
-                    <li><Link to="pages">Pages</Link></li>
-                    <li><Link to="contact">Contact</Link></li>
-                </ul>
+            <div className="header">
+                <div className="row">
+                    <div className="logo">
+                        <h1>SITE NAME</h1>
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <li><Link to="index" activeClassName="active">Home</Link></li>
+                            <li><Link to="about" activeClassName="active">About</Link></li>
+                            <li><Link to="services" activeClassName="active">Services</Link></li>
+                            <li><Link to="work" activeClassName="active">Work</Link></li>
+                            <li><Link to="pages" activeClassName="active">Pages</Link></li>
+                            <li><Link to="contact" activeClassName="active">Contact</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="clear"></div>
             </div>
-        );
+        )
     }
 });
 
