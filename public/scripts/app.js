9ffb72bc6d9fe0198a30d527cdf9f8a870519374
Denis Nefedov <nefedov_de@soshace.com>
2016-01-13 19:11:13 +0300

    Header and footer added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,10 +7,10 @@ var App = React.createClass({
     render: function() {
         return (
             <div className="appComponent">
-                <div className="appHeader">Header</div>
+                <SocialHeader />
                 <SiteMenu />
                 {this.props.children}
-                <div className="appFooter">Footer</div>
+                <Footer />
             </div>
         )
     }
@@ -77,6 +77,67 @@ var Contact = React.createClass({
     render: function() {
         return (
             <div><h1>Contact</h1></div>
+        )
+    }
+});
+
+var SocialHeader = React.createClass({
+    render: function() {
+        return (
+            <div className="social-wrapper">
+                <div className="row">
+                    <ul>
+                        <li><a href="#"><img src="images/social-1.jpg" alt="" /></a></li>
+                        <li><a href="#"><img src="images/social-2.jpg" alt="" /></a></li>
+                        <li><a href="#"><img src="images/social-3.jpg" alt="" /></a></li>
+                        <li className="no-padding"><a href="#"><img src="images/social-4.jpg" alt="" /></a></li>
+                    </ul>
+                    <div className="clear"></div>
+                </div>
+            </div>
+        )
+    }
+});
+
+var Footer = React.createClass({
+    render: function() {
+        return (
+            <div className="footer-wrapper">
+                <div className="footer">
+                    <div className="panel">
+                        <div className="title">
+                            <h1>ABOUT US</h1>
+                            <h2>Aliquam id felis vitae tellus</h2>
+                            <div className="content">
+                                <p>Becenas neque ante,  congue condim
+                                    entum ipsum. Integer et enim a massa dictum conguebitur tempor.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="panel">
+                        <div className="title">
+                            <h1>CONTACT US</h1>
+                            <h2>Ipsum vestibulum non ferme</h2>
+                            <div className="content">
+                                <p><a href="info@sitename.com">info@sitename.com</a> </p>
+                                <h3>( 000 ) 888 8888</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="panel border-right">
+                        <div className="title">
+                            <h1>COPY RIGHT</h1>
+                            <h2>Ultrices dictum etut urna.</h2>
+                            <div className="content">
+                                <p>websitename. All rights reserved. </p>
+                                <p><a href="www.alltemplateneeds.com" target="_blank" className="active">www.alltemplateneeds.com</a> </p>
+                                <p>Images by: <a href="www.photorack.net" target="_blank"> www.photorack.net</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="clear"></div>
+            </div>
         )
     }
 });
