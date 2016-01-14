be8cf945b61a654881c3885b3d88477663fcb37f
Denis Nefedov <nefedov_de@soshace.com>
2016-01-14 12:44:47 +0300

    Contact application form was modified

--- public/scripts/components/home.js
+++ public/scripts/components/home.js
@@ -168,7 +168,108 @@ window.News = React.createClass({
     }
 });
 
+window.ContactForm = React.createClass({
+    getInitialState: function() {
+        return {name: '', email: '', message: ''};
+    },
+    handleNameChange: function(event) {
+        this.setState({name: event.target.value});
+    },
+    handleEmailChange: function(event) {
+        this.setState({email: event.target.value});
+    },
+    handleMessageChange: function(event) {
+        this.setState({message: event.target.value});
+    },
+    handleSubmit: function(event) {
+        event.preventDefault();
+        var name    = this.state.name.trim(),
+            email   = this.state.email.trim(),
+            message = this.state.message.trim();
+
+        if (!name || !email || !message) {
+            // Since there is no error message design mockup console.warn() and alert() were used
+            console.warn('One or more fields are empty');
+            alert('One or more fields are empty');
+            return;
+        }
+        if (!this.validateEmail(email)) {
+            // Since there is no error message design mockup console.warn() and alert() were used
+            console.warn('Email is incorrect');
+            alert('Email is incorrect');
+            return;
+        }
+
+        this.props.onContactSubmit({name: name, email: email, message: message});
+        this.setState({name: '', email: '', message: ''})
+    },
+    validateEmail: function (email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    },
+    render: function() {
+        return (
+            <div>
+                <div className="form">
+                    <ul>
+                        <li>
+                            <input
+                                type="text"
+                                className="text-field"
+                                placeholder="your name here"
+                                value={this.state.name}
+                                onChange={this.handleNameChange}
+                            />
+                        </li>
+                        <li>
+                            <input
+                                type="text"
+                                className="text-field"
+                                placeholder="your email here"
+                                value={this.state.email}
+                                onChange={this.handleEmailChange}
+                            />
+                        </li>
+                        <li>
+                            <textarea
+                                cols=""
+                                rows=""
+                                className="textarea"
+                                placeholder="your message here"
+                                value={this.state.message}
+                                onChange={this.handleMessageChange}
+                            />
+                        </li>
+                    </ul>
+                    <div className="clear"></div>
+                </div>
+                <div className="controller">
+                    <div className="buttons">
+                        <h2><a href="#" onClick={this.handleSubmit}>SUBMIT</a></h2>
+                    </div>
+                    <div className="clear"></div>
+                </div>
+            </div>
+        )
+    }
+});
+
 window.ContactPanel = React.createClass({
+    contactUrl: "http://localhost:3000/api/contacts", // hardcoded for test purposes since used another server for backend
+    handleContactSubmit: function(contactApplication) {
+        $.ajax({
+            url: this.contactUrl,
+            dataType: 'json',
+            type: 'POST',
+            data: contactApplication,
+            success: function() {
+                console.log('Contact application was successfully sent');
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.contactUrl, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
         return (
             <div className="contact-panel">
@@ -176,26 +277,7 @@ window.ContactPanel = React.createClass({
                     <h1>PROIN A RISUS</h1>
                     <h2>Vestibulum suscipit nib</h2>
                 </div>
-                <div className="form">
-                    <ul>
-                        <li>
-                            <input type="text" className="text-field" placeholder="your name here" value=""/>
-                        </li>
-                        <li>
-                            <input type="text" className="text-field" placeholder="your email here" value=""/>
-                        </li>
-                        <li>
-                            <textarea cols="" rows="" className="textarea" placeholder="your message here" />
-                        </li>
-                    </ul>
-                    <div className="clear"></div>
-                </div>
-                <div className="controller">
-                    <div className="buttons">
-                        <h2><a href="#">SUBMIT</a></h2>
-                    </div>
-                    <div className="clear"></div>
-                </div>
+                <ContactForm onContactSubmit={this.handleContactSubmit}/>
                 <div className="clear"></div>
             </div>
         )
